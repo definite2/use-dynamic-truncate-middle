eaf074146cd0cd360ce99f47593627a713fbae1e
Defne Eroglu <defne@appsamurai.com>
2022-04-17 13:20:00 +0300

    add truncate from middle helper

---
+++ src/utils/truncateFromMiddle.ts
@@ -0,0 +1,16 @@
+export const truncateFromMiddle = (
+  fullStr: string,
+  strLen: number,
+  separator = '...',
+) => {
+  if (fullStr.length <= strLen) return fullStr;
+  const sepLen = separator.length;
+  const charsToShow = strLen - sepLen;
+  const frontChars = Math.ceil(charsToShow / 2);
+  const backChars = Math.floor(charsToShow / 2);
+  return (
+    fullStr.substr(0, frontChars) +
+    separator +
+    fullStr.substr(fullStr.length - backChars)
+  );
+};
